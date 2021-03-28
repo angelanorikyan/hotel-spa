78bd44427b0a2af16a5ffec9366c7427a0d74954
Angela Norikyan <anorikyan@brooktec.com>
2021-03-28 16:03:59 +0200

    Tab 1 data adding status

---
+++ src/api/tab1Data.js
@@ -0,0 +1,116 @@
+const tab1Data = [
+  {
+    title:'Solo Alojamiento',
+    id:'solo',
+    status: false,
+    include:[
+      {
+        value: 'Sin régimen',
+        id:1,
+      },
+      {
+        value: 'Botella de agua de bienvenida',
+        id:2,
+      }
+    ]
+  },
+  {
+    title:'Alojamiento y Desayuno',
+    id:'limited',
+    status: false,
+    include:[
+      {
+        value:'Desayuno  buffet completo',
+        id:3,
+      }
+    ]
+  },
+  {
+    title:'Media Pensión',
+    id:'medium',
+    status: false,
+    include:[
+      {
+        value: 'Desayuno buffet completo',
+        id:4,
+      },
+      {
+        value: 'Cena buffet',
+        id:5,
+      },
+      {
+        value: 'No incluye',
+        id:6,
+      }
+    ]
+  },
+  {
+    title:'Todo incluido',
+    id:'allInclusive',
+    status: false,
+    include:[
+      {
+        value: 'Dispondrás de comida y bebida todo el día y durante toda tu estancia.',
+        id:7,
+      },
+      {
+        value: 'Restaurante buffet',
+        id:8,
+      },
+      {
+        value: 'Snack Bar',
+        id:9,
+      },
+      {
+        value: 'Servicio de bares (marcas nacionales)',
+        id:10,
+      }
+    ]
+  },
+  {
+    title:'Unlimited Service',
+    id:'unlimited',
+    status: false,
+    include:[
+      {
+        value: 'Restaurante buffet con bebidas Premium.',
+        id:11,
+      },
+      {
+        value: 'Restaurante a la carta (una cena por estancia)',
+        id:12,
+      },
+      {
+        value: 'Snack Bar con bebidas Premium',
+        id:13,
+      },
+      {
+        value: 'Minibar incluido',
+        id:14,
+      },
+      {
+        value: 'Acceso a SPA y 45 minutos de masaje por persona',
+        id:15,
+      },
+      {
+        value: 'Actividades deportivas (acuáticas sin motor)',
+        id:16,
+      },
+      {
+        value: 'Caja fuerte',
+        id:17,
+
+      },
+      {
+        value: 'Parking gratuito',
+        id:18,
+      },
+      {
+        value: 'Lavandería',
+        id:19,
+      },
+    ]
+  },
+]
+
+export default tab1Data;
