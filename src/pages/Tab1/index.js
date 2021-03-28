a08e561ca5d35df6434e2eef8c24474f78fe8677
Angela Norikyan <anorikyan@brooktec.com>
2021-03-28 16:03:12 +0200

    Tab 1 boxes clickable

--- src/pages/Tab1/index.js
+++ src/pages/Tab1/index.js
@@ -1,21 +1,41 @@
+import { useState } from 'react';
+import produce from 'immer';
+
 import tab1Data from '../../api/tab1Data';
+// components
 import Box from '../../components/Box/index.js';
+// style
 import './style.scss';
 
+// images
+import star from '../../assets/imgs/stars.png';
+
 const Tab1 = () => {
+  const [allBoxes, setAllBoxes] = useState(tab1Data);
+
+  const handleBoxClick = (box) => {
+    // const newBoxes = produce(allBoxes, draftBox => {
+    //   draftBox.map(b => b.id === box.id ? ({...box, status: !box.status}) : box)
+    // })
+    setAllBoxes(produce(allBoxes , draftBox => draftBox.map(b => b.id === box.id ? ({...box, status: !box.status}) : b)) )
+
+  }
+
   return (
     <section className='container'>
       <div className='header'>
-        <span>HOTEL Mieres del Cam&iacute;n Apartamentos</span>
-        <img src='../../assets/imgs/stars.png' alt='4-star'/>
+        <span className='title'>HOTEL Mieres del Cam&iacute;n Apartamentos</span>
+        <span> <img className='stars' src={star} alt='4-star'/></span>
       </div>
       <section className='boxContainer'>
-        {tab1Data.map(tab1 => {
-        return <Box key={tab1.id} data={tab1}/>
-        })}
+        {tab1Data.map(tab1 => <Box
+          key={tab1.id}
+          onBoxClick={(box) => handleBoxClick(box)}
+          boxData={tab1}
+        />)}
       </section>
     </section>
   );
 }
- 
+
 export default Tab1;
